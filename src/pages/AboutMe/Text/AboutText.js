065acebea84590533fc24a4b3afce05670c41591
Maria <maria123321borovikova@gmail.com>
2024-04-29 14:25:15 -0300

    main

--- src/pages/AboutMe/Text/AboutText.js
+++ src/pages/AboutMe/Text/AboutText.js
@@ -8,7 +8,10 @@ const AboutText = () => {
                 <h2 className={styles.whiteText}>Hi, my name is Masha!</h2>
             </div>
             <div className={styles.div2}>
-                <h3>I'm a frontend developer based in Spain with a passion for motion design. I'm deeply inspired by digital artist Ben Marriott and his compelling storytelling techniques.
+                <h3  className={styles.p1}>I'm a frontend developer based in Spain with a passion for motion design. I'm deeply inspired by digital artist Ben Marriott and his compelling storytelling techniques.
+                    I enjoy working remotely and am always eager to take on new challenges in this dynamic field.
+                    </h3>
+                    <h3>
                     I enjoy working remotely and am always eager to take on new challenges in this dynamic field.
                     </h3>
             </div>
