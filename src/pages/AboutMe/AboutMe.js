b5c19e5fed5fcd2360f8844e58b417e16961b6d2
Maria <maria123321borovikova@gmail.com>
2024-04-29 23:46:49 -0300

    main

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -3,14 +3,32 @@ import styles from './AboutMe.module.css'; // Import the CSS module
 import AboutText from './Text/AboutText';
 
 const AboutMe = () => {
+    const videoRef = React.useRef(null); // Create a ref for the video element
+
+    const handleMouseEnter = () => {
+        videoRef.current.play(); // Play the video when mouse enters
+    };
+
+    const handleMouseLeave = () => {
+        videoRef.current.pause(); // Pause the video when mouse leaves
+    };
+
     return (
         <div className={styles.aboutmeContainer}>
             <AboutText/>
             <div className={styles.imageContainer}>
-            <img src="eyes.png" alt="animation of dragon with fire" className={styles.image}/>
+                <video
+                    ref={videoRef}
+                    src="dragon.mov" // Specify your video file here
+                    alt="Animation of a dragon with fire"
+                    className={styles.image}
+                    loop // Loop the video if needed
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    muted // Mute the video, required for autoplay on most browsers
+                />
+            </div>
         </div>
-        </div>
-
     );
 }
 
